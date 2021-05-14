7f9185822e408b0afa85d2fd6c82b7d7608fe81b
Jay Mehta <jake2001007@gmail.com>
2021-05-14 12:52:38 +0530

    Added some dummy sections and utility classes 🥱

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,8 @@ export default function Home() {
         <title>Da Meet by Codegrain | Home</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+
+      {/* INTRO SECTION */}
       <section className="intro">
         <div className="container">
           <div className="split">
@@ -37,7 +39,10 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="gray-bg">
+
+      {/* SECOND SECTION */}
+
+      <section className="card-section bg-light eq-space">
         <div className="container">
           <div className="split">
             <div className="flow-content">
@@ -60,6 +65,88 @@ export default function Home() {
             </div>
             <div className="pl">
               <Image src="/second.png" width={500} height={414}></Image>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* THIRD SECTION */}
+
+      <section className="eq-space">
+        <div className="container">
+          <div className="split">
+            <div className="flow-content">
+              <h3>Our 2021 Speakers</h3>
+              <p>We have shakers of the industry</p>
+            </div>
+          </div>
+          <div className="speaker-cards large-space">
+            <div class="speaker bg-light">
+              <img src="/img/team-01.jpg" alt="" />
+              <div class="info">
+                <h3 class="name">Adriana Ramirez</h3>
+                <p class="position">Lead UI/UX</p>
+                <p>Sed sit tellus sit mauris sit hendrerit pellentesque.</p>
+              </div>
+            </div>
+            <div class="speaker bg-light">
+              <img src="/img/team-02.jpg" alt="" />
+              <div class="info">
+                <h3 class="name">Calvin Miles</h3>
+                <p class="position">UI/UX Designer</p>
+                <p>Sed sit tellus sit mauris sit hendrerit pellentesque.</p>
+              </div>
+            </div>
+            <div class="speaker bg-dark speaker--mirrored">
+              <img src="/img/team-03.jpg" alt="" />
+              <div class="info">
+                <h3 class="name">Hardy Smith</h3>
+                <p class="position">Lead developer</p>
+                <p>Sed sit tellus sit mauris sit hendrerit pellentesque.</p>
+              </div>
+            </div>
+
+            <div class="speaker bg-dark speaker--mirrored">
+              <img src="/img/team-04.jpg" alt="" />
+              <div class="info">
+                <h3 class="name">Janae Watts</h3>
+                <p class="position">Front-end developer</p>
+                <p>Sed sit tellus sit mauris sit hendrerit pellentesque.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* FOURTH SECTION */}
+      <section className="bg-light eq-space">
+        <div className="container">
+          <div className="split">
+            <div className="flow-content">
+              <h2>Frequently Asked Questions. 🤔 </h2>
+
+              <div className="flow-content flow-content--small">
+                <h4 className="large-space">
+                  When is The Dain Walker Experience?
+                </h4>
+                <p>The workshop is happening at 5pm EST on May 12th</p>
+                <h4>How do I attend?</h4>
+                <p>
+                  We built our own platform! No Youtube links, no Zoom calls.
+                  Simply sign up and everything is there!
+                </p>
+                <h4>What if I miss the session?</h4>
+                <p>
+                  We get it, things get in the way. If you have a VIP pass, you
+                  can rewatch sessions at any time.
+                </p>
+                <h4>What is Frontrow2021?</h4>
+                <p>
+                  Frontrow is a virtual conference happening on May 10 - 14
+                  featuring top designers like Chris Do. Workshops, mentoring,
+                  networking & so much more!
+                </p>
+              </div>
             </div>
           </div>
         </div>
