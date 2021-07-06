05a3a63b338f8a3d30df7651ec96400db93f7dae
Jay Mehta <jake2001007@gmail.com>
2021-07-06 20:27:48 +0530

    Added site analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
 import { UserProvider } from "@auth0/nextjs-auth0";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <>
+      <Head>
+        <script>
+          if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!==
+          0)
+          {fetch(
+            "https://counter.dev/track?" +
+              new URLSearchParams({
+                referrer: document.referrer,
+                screen: screen.width + "x" + screen.height,
+                user: "codegrain",
+                utcoffset: "6",
+              })
+          )}
+          ;sessionStorage.setItem("_swa","1");
+        </script>
+      </Head>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </>
   );
 }
 
