99dde21901acefd9b1b5599cba17b6614157f8a7
Jay Mehta <jake2001007@gmail.com>
2021-07-09 17:52:13 +0530

    Added schedule to schedule page (lol)

--- pages/schedule.js
+++ pages/schedule.js
@@ -24,49 +24,72 @@ const Schedule = () => {
                 <h5>Plan your schedule!!</h5>
               </div>
             </section>
-            {/* <div className="split">
+            <div className="split">
               <div className="flow-content flow-content--large bg-light event-card">
                 <div className="a-self">
                   <h3 className="text-center">July 17</h3>
                   <div className="small-space a-self">
                     <Image
                       quality={100}
-                      src="/img/speaker-01-alt.png"
+                      src="/img/speaker-2-alt.png"
                       width={220}
                       height={220}
                       alt="speaker"
                     />
                     <h4 className="small-space text-center">
-                      Session 1: Intro to UX by Saish Gosavi
+                      Intro to Branding and Logo Designing
                     </h4>
-                    <p className="text-center">Category | 4: 30 PM</p>
+                    <p className="text-center">
+                      Shubhang Rajput |{" "}
+                      <span className="bold">3:00-4:30 PM</span>
+                    </p>
                   </div>
                 </div>
                 <div className="a-self">
                   <Image
                     quality={100}
-                    src="/img/speaker-2-alt.png"
+                    src="/img/speaker-5-alt.png"
                     width={220}
                     height={220}
                     alt="speaker"
                   />
                   <h4 className="small-space text-center">
-                    Session 2: Title and speaker name
+                    Show your work: Get discovered and fly high as a developer
                   </h4>
-                  <p className="text-center">Category | 6: 30 PM</p>
+                  <p className="text-center">
+                    Cris Pintea | <span className="bold">5:00-6:00 PM</span>
+                  </p>
                 </div>
                 <div className="a-self">
                   <Image
                     quality={100}
-                    src="/img/group-1.png"
+                    src="/img/speaker-4-alt.png"
+                    width={220}
+                    height={220}
+                    alt="speaker"
+                  />
+                  <h4 className="small-space text-center">
+                    Intro to App Development
+                  </h4>
+                  <p className="text-center">
+                    Roshan Singh | <span className="bold">6:30-8:00 PM</span>
+                  </p>
+                </div>
+                <div className="a-self">
+                  <Image
+                    quality={100}
+                    src="/img/collab.png"
                     width={325}
                     height={200}
                     alt="speaker"
                   />
                   <h4 className="small-space text-center">
-                    Session 3: Title and speaker name
+                    How & Why to start a side gig early on?
                   </h4>
-                  <p className="text-center">Category | 6: 30 PM</p>
+                  <p className="text-center">
+                    Shubhang + Roshan |{" "}
+                    <span className="bold">8:15-9:00 PM</span>
+                  </p>
                 </div>
               </div>
               <div className="flow-content flow-content--large  bg-light event-card">
@@ -81,27 +104,45 @@ const Schedule = () => {
                       alt="speaker"
                     />
                     <h4 className="small-space text-center">
-                      Session 1: Title and speaker names
+                      Web Development Basics
                     </h4>
-                    <p className="text-center">Category | 4: 30 PM</p>
+                    <p className="text-center">
+                      Janvi Poojary | <span className="bold">3:00-4:30 PM</span>
+                    </p>
                   </div>
                 </div>
                 <div className="a-self">
                   <Image
                     quality={100}
-                    src="/img/speaker-4-alt.png"
+                    src="/img/speaker-6-alt.png"
                     width={220}
                     height={220}
                     alt="speaker"
                   />
                   <h4 className="small-space text-center">
-                    Session 2: Title and speaker name
+                    How to get clients as a freelancer/beginner?
                   </h4>
-                  <p className="text-center">Category | 6: 30 PM</p>
+                  <p className="text-center">
+                    Pranjal Doorwar | <span className="bold">5:00-6:30 PM</span>
+                  </p>
+                </div>
+                <div className="a-self">
+                  <Image
+                    quality={100}
+                    src="/img/speaker-01-alt.png"
+                    width={220}
+                    height={220}
+                    alt="speaker"
+                  />
+                  <h4 className="small-space text-center">
+                    Intro to User Experience
+                  </h4>
+                  <p className="text-center">
+                    Saish Gosavi | <span className="bold">7:00-8:30 PM</span>
+                  </p>
                 </div>
               </div>
-            </div> */}
-            <h3>Coming Soon!!!</h3>
+            </div>
             <section className="info-grid small-space">
               <div className="info-card-row info-cards bg-light">
                 <div>
